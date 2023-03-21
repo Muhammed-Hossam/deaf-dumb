f43a7d52872ee8f2617dba652dfc39450dc13454
Muhamed-hossam <developer.muhamed@gmail.com>
2023-03-21 12:47:15 +0200

    Refactor Header Responsive Issue

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,38 @@
 <template>
   <header>
+    <div
+      v-if="isLoggedIn"
+      class="greeting-user greeting-user-mobile dropdown text-center py-3 bg-light"
+      style="position: unset"
+    >
+      <span class="userName fw-bold me-2"
+        ><span class="text-dark">Hi,</span> {{ userName }}</span
+      >
+      <button
+        class="user-img rounded-circle border d-inline-block fs-3"
+        type="button"
+        @click="clickOnUserImg"
+      >
+        <font-awesome-icon v-if="!userPhoto" icon="fa-solid fa-user" />
+        <img v-else :src="userPhoto" class="user-photo" alt="" />
+      </button>
+      <ul
+        class="dropdown-menu text-center end-0"
+        :class="{ show: isUserImgClicked }"
+        style="position: unset !important"
+      >
+        <li>
+          <button class="btn p-3 w-100 rounded-0" @click="logoutUser">
+            Logout
+          </button>
+        </li>
+        <li v-if="role === 'admin'">
+          <router-link class="dropdown-item" to="/dashboard"
+            >Dashboard</router-link
+          >
+        </li>
+      </ul>
+    </div>
     <nav class="navbar navbar-expand-lg bg-body-tertiary p-0">
       <div class="container-fluid">
         <router-link to="/" class="navbar-brand py-0">
@@ -53,7 +86,9 @@
                   >
                 </li>
                 <li>
-                  <router-link class="dropdown-item text-muted" to="/common-signs"
+                  <router-link
+                    class="dropdown-item text-muted"
+                    to="/common-signs"
                     >Common Signs</router-link
                   >
                 </li>
@@ -63,14 +98,18 @@
                   >
                 </li>
                 <li>
-                  <router-link class="dropdown-item text-muted" to="/flash-cards"
+                  <router-link
+                    class="dropdown-item text-muted"
+                    to="/flash-cards"
                     >Flash Cards</router-link
                   >
                 </li>
               </ul>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/contact">Contact Us</router-link>
+              <router-link class="nav-link" to="/contact"
+                >Contact Us</router-link
+              >
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/about">About Us</router-link>
@@ -79,15 +118,21 @@
               <router-link class="nav-link" to="/privacy">Privacy</router-link>
             </li>
           </ul>
-          <ul v-if="!isLoggedIn" class="navbar-nav auth-links" role="navigation">
+          <ul
+            v-if="!isLoggedIn"
+            class="navbar-nav auth-links"
+            role="navigation"
+          >
             <li class="nav-item">
-              <router-link class="nav-link" to="/register">Register</router-link>
+              <router-link class="nav-link" to="/register"
+                >Register</router-link
+              >
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/login">Login</router-link>
             </li>
           </ul>
-          <div v-else class="user-info dropdown">
+          <div v-else class="greeting-user dropdown greeting-user-desktop d-none">
             <span class="userName fw-bold me-2"
               ><span class="text-dark">Hi,</span> {{ userName }}</span
             >
@@ -100,7 +145,7 @@
               <font-awesome-icon v-if="!userPhoto" icon="fa-solid fa-user" />
               <img v-else :src="userPhoto" class="user-photo" alt="" />
             </button>
-            <ul class="dropdown-menu text-center">
+            <ul class="dropdown-menu text-center end-0">
               <li>
                 <button class="btn p-3 w-100 rounded-0" @click="logoutUser">
                   Logout
@@ -126,7 +171,9 @@ import { mapGetters } from "vuex";
 
 export default {
   data() {
-    return {};
+    return {
+      isUserImgClicked: false,
+    };
   },
 
   computed: {
@@ -149,6 +196,9 @@ export default {
         .catch((error) => {
           console.error(error.message);
         });
+    },
+    clickOnUserImg() {
+      this.isUserImgClicked = !this.isUserImgClicked;
     },
     /*
       loop through navbar links and when click on any link of them add "active" class to clicked link 
@@ -198,44 +248,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.navbar {
-  .user-info {
-    .user-img {
+.greeting-user {
+  .user-img {
+    width: 50px;
+    height: 50px;
+    text-align: center;
+    line-height: 50px;
+    transition: all 0.3s ease-in-out;
+    border: 5px solid #ccc;
+    cursor: pointer;
+    &:hover,
+    &:focus {
+      box-shadow: 0 1px 3px 5px rgb(0 0 0 / 10%);
+    }
+
+    .user-photo {
       width: 50px;
       height: 50px;
-      text-align: center;
-      line-height: 50px;
-      transition: all 0.3s ease-in-out;
-      border: 5px solid #ccc;
-      cursor: pointer;
-      &:hover,
-      &:focus {
-        box-shadow: 0 1px 3px 5px rgb(0 0 0 / 10%);
-      }
-
-      .user-photo {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        transform: translate(-6px, -4px);
-        border: 2px solid #ccc;
-      }
-    }
-    .userName {
-      color: #02b1b4;
-      vertical-align: super;
-    }
-
-    .dropdown-menu {
-      margin-top: 0.7rem;
-      button {
-        transition: all 0.3s ease-in-out;
-      }
-      button:hover {
-        background-color: #ddd;
-      }
+      border-radius: 50%;
+      transform: translate(-6px, -4px);
+      border: 2px solid #ccc;
     }
   }
+  .userName {
+    color: #02b1b4;
+    vertical-align: super;
+  }
+
+  .dropdown-menu {
+    margin-top: 0.7rem;
+    button {
+      transition: all 0.3s ease-in-out;
+    }
+    button:hover {
+      background-color: #ddd;
+    }
+  }
+}
+.navbar {
   .navbar-brand {
     color: #02b1b4;
     transition: all 0.5s ease-in-out;
@@ -303,24 +353,30 @@ export default {
     //   width: 85%;
     // }
   }
-  @media (min-width: 992px) {
-    .nav-link,
-    .dropdown-item {
-      &.router-link-exact-active {
-        &::after {
-          width: 85%;
-        }
-      }
-      &:hover::after {
+}
+@media (min-width: 992px) {
+  .greeting-user-mobile {
+    display: none !important;
+  }
+  .greeting-user-desktop {
+    display: block !important;
+  }
+  .nav-link,
+  .dropdown-item {
+    &.router-link-exact-active {
+      &::after {
         width: 85%;
       }
     }
+    &:hover::after {
+      width: 85%;
+    }
+  }
 
-    .dropdown {
-      &:hover {
-        .dropdown-menu.contents {
-          display: block;
-        }
+  .dropdown {
+    &:hover {
+      .dropdown-menu.contents {
+        display: block;
       }
     }
   }
